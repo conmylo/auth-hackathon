10f4ab5e081c05a9c2e210e5f0f8e59c07626d42
Charalampos Kemalidis <kemalidic@ece.auth.gr>
2023-12-22 14:48:28 +0200

    tests for US2

--- cypress/e2e/hackathon-tests/complete_order.cy.ts
+++ cypress/e2e/hackathon-tests/complete_order.cy.ts
@@ -1,14 +1,81 @@
-describe("Add Products in the Cart", () => {
+describe("Complete Order for the products in the cart", () => {
     beforeEach(() => {
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
     })
 
-    it('Add product', () => {
-        cy.get('.product-image').should('exist')
-        cy.get('.products.loading').should('not.exist')
-        cy.get('.products').each($product => {
-            let price: string = $product.find('p.product-price').text()
-            cy.log(price, $product)
+    it('Completes an order with more than one item in the cart', () => {
+        cy.get('.products').find('.loadind').should('not.exist')
+        cy.get('.products').find('.product').each($product => {
+            cy.wrap($product.find('.product-action > button')).should('have.text', 'ADD TO CART')
+            cy.wrap($product.find('.product-action > button')).click()
         })
+
+        cy.get('.cart .cart-icon > img').click()
+        cy.get('.cart-preview.active > .action-block > button').click()
+
+        cy.url().should('include', '/cart')
+
+        cy.get('button').contains('Place Order').click()
+
+        cy.url().should('include', '/country')
+
+        cy.get('select').select('Greece').should('have.value', 'Greece')
+
+        cy.get('.chkAgree').check().should('be.checked')
+
+        cy.get('button').contains('Proceed').click()
+
+        cy.get('span').contains('Thank you, your order has been placed successfully')
+    })
+
+    it('Tests if an order can be completed with no items in the cart', () => {
+        cy.get('.cart .cart-icon > img').click()
+        cy.get('.cart-preview.active > .action-block > button').click()
+
+        cy.url().should('include', '/cart')
+
+        cy.get('.empty-cart > h2').should('have.text', 'You cart is empty!')
+
+        cy.get('button').contains('Place Order').should('be.disabled')
+    })
+
+    it('Tests if Terms & Conditions is mandatory for completing an order', () => {
+        cy.get('.products').find('.loadind').should('not.exist')
+        cy.get('.products').find('.product').first().find('.product-action > button').should('have.text', 'ADD TO CART').click()
+
+        cy.get('.cart .cart-icon > img').click()
+        cy.get('.cart-preview.active > .action-block > button').click()
+
+        cy.url().should('include', '/cart')
+
+        cy.get('button').contains('Place Order').click()
+
+        cy.url().should('include', '/country')
+
+        cy.get('select').select('Greece').should('have.value', 'Greece')
+
+        cy.get('button').contains('Proceed').click()
+
+        cy.get('.errorAlert > b').should('have.text', 'Please accept Terms & Conditions - Required')
+    })
+
+    it('Tests if an order can be completed when no country is sellected', () => {
+        cy.get('.products').find('.loadind').should('not.exist')
+        cy.get('.products').find('.product').first().find('.product-action > button').should('have.text', 'ADD TO CART').click()
+
+        cy.get('.cart .cart-icon > img').click()
+        cy.get('.cart-preview.active > .action-block > button').click()
+
+        cy.url().should('include', '/cart')
+
+        cy.get('button').contains('Place Order').click()
+
+        cy.url().should('include', '/country')
+
+        cy.get('select').should('have.value', null)
+
+        cy.get('.chkAgree').check().should('be.checked')
+
+        cy.get('button').should('be.disabled')
     })
 })
