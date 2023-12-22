19547e0684b708797d29a8480ef754cc7896a5dd
Ioannis Horgos <jhor@acta.com.gr>
2023-12-22 14:49:42 +0200

    implemented test steps

--- cypress/e2e/hackathon-tests/product_cart.cy.ts
+++ cypress/e2e/hackathon-tests/product_cart.cy.ts
@@ -1,27 +1,65 @@
 describe("Add Products in the Cart", () => {
-    beforeEach(() => {
+    before(() => {
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
     })
 
+    let products_length = 0
+    let quantity: number = 0;
     let total_price = 0
-    it('Add product', () => {
+    it('Add product in the cart', () => {
+
         cy.get('.products').find('.loading').should('not.exist')
 
-        cy.get('.products').find('.product').each($product => {
+        cy.get('.products').find('.product').each(($product, index) => {
+            products_length++;
             let price: number = +$product.find('p.product-price').text()
-            let quantity: number;
-            cy.wrap($product.find('.increment')).click()
+
+            cy.get('.cart-preview.active').should('not.exist')
+            cy.get('.cart-icon').click();
+            cy.get('.cart-preview.active').should('exist')
+            if (index === 0)
+                cy.get('.empty-cart').should('exist')
+            cy.get('.cart-icon').click();
+
+
+            const random_quantities = Math.floor(Math.random() * 1);
+            for (let i = 0; i < random_quantities; i++)
+                cy.wrap($product.find('.increment')).click()
+
             cy.wrap($product.find('.stepper-input .quantity'))
                 .invoke('val')
-                .then((inputVal: string | string[] | null) => quantity = +inputVal)
+                .then((inputVal: string | string[] | null) => {
+                    quantity += +inputVal
+                    total_price += +inputVal * price
+                })
 
-            // cy.log(price, $product, quantity)
             cy.wrap($product.find('.product-action > button')).should('have.text', 'ADD TO CART')
             cy.wrap($product.find('.product-action > button')).click()
             cy.wrap($product.find('.product-action > button')).should('have.text', '✔ ADDED')
-            total_price += quantity * price
+
+            let product_price = quantity * price
 
         })
+    })
+
+    it('Total Items and Price', () => {
+        cy.get('.cart-info > table > tbody > tr').each($tr => {
+            const tds = $tr.find('td')
+
+            if (tds.eq(0).text().includes('Items')) {
+                cy.wrap(tds).should('have.text', `Items:${products_length * quantity}`)
+            }
+            else if (tds.eq(0).text().includes('Price')) {
+                cy.wrap(tds).should('have.text', `Price:${total_price}`)
+            }
+        })
+
+        cy.get('ul.cart-items > li.cart-item')
+            .should('not.have.class', 'fadeIn-enter')
+            .should('not.have.class', 'fadeIn-enter-active')
+
+        // cy.get('ul.cart-items').each($li => cy.log($li))
+        cy.get('ul.cart-items').should('have.length', products_length)
 
     })
 })
